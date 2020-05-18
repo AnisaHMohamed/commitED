745e95c7922ccb0ba905f3fd2caa47ed8fe142f9
Anisa <anisahamohamed@gmail.com>
2020-05-18 19:19:31 -0400

    add es6 cardfooter component refractor

--- frontend/src/components/Card/CardFooter.js
+++ frontend/src/components/Card/CardFooter.js
@@ -12,21 +12,23 @@ import styles from "../../assets/jss/material-kit-react/components/cardFooterSty
 
 const useStyles = makeStyles(styles);
 
-export default function CardFooter(props) {
+const CardFooter = (props) => {
   const classes = useStyles();
   const { className, children, ...rest } = props;
   const cardFooterClasses = classNames({
     [classes.cardFooter]: true,
-    [className]: className !== undefined
+    [className]: className !== undefined,
   });
   return (
     <div className={cardFooterClasses} {...rest}>
       {children}
     </div>
   );
-}
+};
 
 CardFooter.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.node
+  children: PropTypes.node,
 };
+
+export default CardFooter;
