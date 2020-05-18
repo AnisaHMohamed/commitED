46d61e9c0ec4c89c6e0a9b34ea8a9fc395967479
Anisa <anisahamohamed@gmail.com>
2020-05-18 19:43:40 -0400

    add es6 login component refractor

--- frontend/src/components/LoginForm/LoginForm.js
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -5,14 +5,12 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
 import Email from "@material-ui/icons/Email";
-import People from "@material-ui/icons/People";
 // core components
 import Button from "../CustomButtons/Button";
 import Card from "../Card/Card";
 import CardBody from "../Card/CardBody";
 import CardFooter from "../Card/CardFooter";
 import CustomInput from "../CustomInput/CustomInput";
-import FormControl from "@material-ui/core/FormControl";
 import axios from "axios";
 import qs from "qs";
 
@@ -20,14 +18,9 @@ import styles from "../../assets/jss/material-kit-react/views/loginPage";
 
 const useStyles = makeStyles(styles);
 
-export default function LoginPage(props) {
-  const [cardAnimaton, setCardAnimation] = useState("cardHidden");
-  // setTimeout(function () {
-  //   setCardAnimation("");
-  // }, 700);
+const LoginPage = (props) => {
   const classes = useStyles();
   const { ...rest } = props;
-  const [message, setMessage] = useState('')
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -37,67 +30,63 @@ export default function LoginPage(props) {
         "/api/login",
         qs.stringify({
           email: e.target.email.value,
-          password: e.target.pass.value
+          password: e.target.pass.value,
         })
       )
-      .then(response => {
-        // todo: remove once you're all set up *** add in if you want to render serviceUSER
-        console.log(response)
-        localStorage.setItem('user', JSON.stringify(response.data.user));
+      .then((response) => {
+        localStorage.setItem("user", JSON.stringify(response.data.user));
         props.setUser(response.data.user);
         window.location = "/index";
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
+    return (
+      <Card>
+        <form onSubmit={handleSubmit}>
+          <CardBody>
+            <CustomInput
+              labelText="Email..."
+              id="email"
+              formControlProps={{
+                fullWidth: true,
+              }}
+              inputProps={{
+                type: "email",
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Email className={classes.inputIconsColor} />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <CustomInput
+              labelText="Password"
+              id="pass"
+              formControlProps={{
+                fullWidth: true,
+              }}
+              inputProps={{
+                type: "password",
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Icon className={classes.inputIconsColor}>
+                      {/* lock_outline */}
+                    </Icon>
+                  </InputAdornment>
+                ),
+                autoComplete: "off",
+              }}
+            />
+          </CardBody>
+          <CardFooter className={classes.cardFooter}>
+            <Button type="submit" simple color="primary" size="lg">
+              Submit
+            </Button>
+          </CardFooter>
+        </form>
+      </Card>
+    );
   }
-
-  //Return
-
-  return (
-    <Card>
-      <form onSubmit={handleSubmit}>
-        <CardBody>
-          <CustomInput
-            labelText="Email..."
-            id="email"
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              type: "email",
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Email className={classes.inputIconsColor} />
-                </InputAdornment>
-              )
-            }}
-          />
-          <CustomInput
-            labelText="Password"
-            id="pass"
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              type: "password",
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Icon className={classes.inputIconsColor}>
-                    {/* lock_outline */}
-                  </Icon>
-                </InputAdornment>
-              ),
-              autoComplete: "off"
-            }}
-          />
-        </CardBody>
-        <CardFooter className={classes.cardFooter}>
-          <Button type="submit" simple color="primary" size="lg">
-            Submit
-          </Button>
-        </CardFooter>
-      </form>
-    </Card>
-  );
-}
+};
+export default LoginPage;
