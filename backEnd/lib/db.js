9946efa3fae4a8b00fac85ae27ae77fd17db48a6
Anisa <anisahamohamed@gmail.com>
2020-05-18 17:32:21 -0400

    database refractor and comments

--- backEnd/lib/db.js
+++ backEnd/lib/db.js
@@ -1,5 +1,7 @@
-const { Pool } = require('pg');
+//Database Connection for setup
+const { Pool } = require("pg");
 let dbParams = {};
+
 if (process.env.DATABASE_URL) {
   dbParams.connectionString = process.env.DATABASE_URL;
 } else {
@@ -8,56 +10,115 @@ if (process.env.DATABASE_URL) {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
   };
 }
 //create queries here and assign them to functions
 const db = new Pool(dbParams);
 db.connect();
+
 const createRequest = (opportunity_id, user_id, status) => {
   return db.query(
     `INSERT INTO requests(opportunity_id, user_id, status) VALUES($1,$2, $3) RETURNING *;`,
-    [opportunity_id, user_id, status])//user.id //props.id==> opportunity id;
+    [opportunity_id, user_id, status]
+  );
 };
-const showRequests = (user_id, opportunity_id) =>{
-return  db.query(
-  `SELECT * FROM requests WHERE user_id = $1 AND opportunity_id = $2`,
-[user_id ,opportunity_id])
-}
-const createPost = (type,description, title, date_posted, user_id, address, longitude, latitude) => {
-  return db.query(`INSERT INTO opportunities(type, description, title, date_posted, user_id, address,longitude, latitude) VALUES($1,$2,$3,to_timestamp($4),$5,$6,$7,$8) RETURNING *;`,
-  [type,description, title, date_posted, user_id, address, longitude, latitude]
-  )
-}
-const showPosts = () =>{
+
+const showRequests = (user_id, opportunity_id) => {
   return db.query(
-    `SELECT * FROM opportunities;`)
-}
+    `SELECT * FROM requests WHERE user_id = $1 AND opportunity_id = $2`,
+    [user_id, opportunity_id]
+  );
+};
+
+const createPost = (
+  type,
+  description,
+  title,
+  date_posted,
+  user_id,
+  address,
+  longitude,
+  latitude
+) => {
+  return db.query(
+    `INSERT INTO opportunities(type, description, title, date_posted, user_id, address,longitude, latitude) VALUES($1,$2,$3,to_timestamp($4),$5,$6,$7,$8) RETURNING *;`,
+    [
+      type,
+      description,
+      title,
+      date_posted,
+      user_id,
+      address,
+      longitude,
+      latitude,
+    ]
+  );
+};
+
+const showPosts = () => {
+  return db.query(`SELECT * FROM opportunities;`);
+};
+
 const login = (email) => {
- return db.query(`SELECT id, email, password, type
+  return db.query(
+    `SELECT id, email, password, type
       FROM users
-      WHERE email = $1;`, [email])
-}
-const getEmail = (email) =>{
-  return db.query(`SELECT email
+      WHERE email = $1;`,
+    [email]
+  );
+};
+const getEmail = (email) => {
+  return db.query(
+    `SELECT email
   FROM users
-  WHERE email = $1;`, [email])
-}
-const createUser = (name, address, phone,email, hashedPassword, type, lat, lng) => {
- return  db.query(`INSERT INTO users(name, address, phone_number, email, password, type,latitude, longitude) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *;`,
-        [name, address, phone,email, hashedPassword, type, lat, lng])
-}
+  WHERE email = $1;`,
+    [email]
+  );
+};
+
+const createUser = (
+  name,
+  address,
+  phone,
+  email,
+  hashedPassword,
+  type,
+  lat,
+  lng
+) => {
+  return db.query(
+    `INSERT INTO users(name, address, phone_number, email, password, type,latitude, longitude) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *;`,
+    [name, address, phone, email, hashedPassword, type, lat, lng]
+  );
+};
+
 const getUserLatAndLng = (userId) => {
-  return db.query(`SELECT latitude, longitude
+  return db.query(
+    `SELECT latitude, longitude
                     FROM users
-                    WHERE id = $1;`, [userId])
-}
+                    WHERE id = $1;`,
+    [userId]
+  );
+};
+
 const getOpportunityLatAndLng = () => {
   return db.query(`SELECT id, latitude, longitude
-                    FROM opportunities`)
-}
-const updateOpportunity = ({id, type, description, title, address, longitude, latitude, user_id}) =>{
-  return db.query(`
+                    FROM opportunities`);
+};
+
+const updateOpportunity = ({
+  id,
+  type,
+  description,
+  title,
+  address,
+  longitude,
+  latitude,
+  user_id,
+}) => {
+  return db.query(
+    `
     UPDATE
       opportunities
     SET
@@ -72,37 +133,64 @@ const updateOpportunity = ({id, type, description, title, address, longitude, la
       id = $8
     RETURNING *;`,
     [type, description, title, address, longitude, latitude, user_id, id]
-    )
-  }
-const deleteOpportunities = (user_id, opportunity_id) =>{
-  return db.query(`
+  );
+};
+
+const deleteOpportunities = (user_id, opportunity_id) => {
+  return db.query(
+    `
     DELETE FROM
       opportunities
     WHERE
       user_id = $1
     AND
       id = $2
-    `, [user_id, opportunity_id])
-}
+    `,
+    [user_id, opportunity_id]
+  );
+};
+
 const getEmailOfOpportunityOwner = (user_id) => {
-  return db.query(`
+  return db.query(
+    `
   SELECT * FROM opportunities
-  JOIN users ON users.id = opportunities.user_id WHERE opportunities.user_id = $1`,[user_id]);
+  JOIN users ON users.id = opportunities.user_id WHERE opportunities.user_id = $1`,
+    [user_id]
+  );
 };
 
 const getUserInfoFromId = (id) => {
-  return db.query(`SELECT *
+  return db.query(
+    `SELECT *
        FROM users
-       WHERE id = $1;`, [id]);
+       WHERE id = $1;`,
+    [id]
+  );
 };
+
 const getAllUserOpportunites = (type, email) => {
   return db.query(
-  `SELECT * FROM  Opportunity
+    `SELECT * FROM  Opportunity
   JOIN user ON Opportunity.user_id == user.id
   JOIN requests ON Opportunity.user_id == requests.user_id
   WHERE type  LIKE '%${type}' -- volunteer
-  AND user.email LIKE '%${email}'--email address`)
+  AND user.email LIKE '%${email}'--email address`
+  );
+};
 
-}
-module.exports = { dbParams, createRequest, showRequests, createPost, showPosts,login, getEmail, createUser, getUserLatAndLng, getOpportunityLatAndLng, updateOpportunity, deleteOpportunities, getEmailOfOpportunityOwner,getUserInfoFromId };
-
+module.exports = {
+  dbParams,
+  createRequest,
+  showRequests,
+  createPost,
+  showPosts,
+  login,
+  getEmail,
+  createUser,
+  getUserLatAndLng,
+  getOpportunityLatAndLng,
+  updateOpportunity,
+  deleteOpportunities,
+  getEmailOfOpportunityOwner,
+  getUserInfoFromId,
+};
