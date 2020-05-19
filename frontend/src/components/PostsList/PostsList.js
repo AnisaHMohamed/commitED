5524c584b0f54441b9625a3460959d7da9a3b6a1
Anisa <anisahamohamed@gmail.com>
2020-05-18 21:12:17 -0400

    Es6 refractor and comments on postlist components

--- frontend/src/components/PostsList/PostsList.js
+++ frontend/src/components/PostsList/PostsList.js
@@ -1,21 +1,14 @@
 import React, { useState } from "react";
 import PopupPost from "./PopupPost";
 import Post from "./Post";
-import Roboto from '../../assets/jss/material-kit-react/components/typographyStyle'
+import Roboto from "../../assets/jss/material-kit-react/components/typographyStyle";
 
-
-export default function PostsList({
-  getPosts,
-  posts,
-  user,
-}) {
+const PostsList = ({ getPosts, posts, user }) => {
   const [postToShow, setPostToShow] = useState();
 
   if (user) {
-    const serviceProviderPosts = posts.filter(post => {
-      return (
-        user.type === "service_provider" && post.user_id === user.id
-      );
+    const serviceProviderPosts = posts.filter((post) => {
+      return user.type === "service_provider" && post.user_id === user.id;
     });
 
     const volunteerPosts = posts;
@@ -28,27 +21,32 @@ export default function PostsList({
 
   return (
     <React.Fragment>
-      <div style={{fontFamily: Roboto, fontSize: 18, textAlign: "center", color:"#4D4D4D"}}>
+      <div
+        style={{
+          fontFamily: Roboto,
+          fontSize: 18,
+          textAlign: "center",
+          color: "#4D4D4D",
+        }}
+      >
         <h1>OPPORTUNITIES</h1>
-      <ul style={{listStyleType: 'none', padding: 0, margin: 0}}>
-        {posts.map(post => (
-          <li key={post.id}>
-            <Post
-              post={post}
-              showPostModal={() => setPostToShow(post)}
-            />
-          </li>
-        ))}
-      </ul>
-      {postToShow && (
-        <PopupPost
-          user={user}
-          post={postToShow}
-          onClose={() => setPostToShow(null)}
-          getPosts={getPosts}
-        />
-      )}
+        <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Post post={post} showPostModal={() => setPostToShow(post)} />
+            </li>
+          ))}
+        </ul>
+        {postToShow && (
+          <PopupPost
+            user={user}
+            post={postToShow}
+            onClose={() => setPostToShow(null)}
+            getPosts={getPosts}
+          />
+        )}
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
+export default PostsList;
