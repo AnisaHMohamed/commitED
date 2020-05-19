ed4fb756b23d4722e2965f32a632c46584b3e155
Anisa <anisahamohamed@gmail.com>
2020-05-18 21:08:36 -0400

    Es6 refractor and comments on post  components

--- frontend/src/components/PostsList/Post.js
+++ frontend/src/components/PostsList/Post.js
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
-import Button from "../CustomButtons/Button"
+import React from "react";
+import Button from "../CustomButtons/Button";
 
-export default function Post({showPostModal, post}) {
-
+const Post = ({ showPostModal, post }) => {
   return (
     <Button
-      style={{borderRadius: "5px" }}
-      height='40px'
-      justify-content='left'
+      style={{ borderRadius: "5px" }}
+      height="40px"
+      justify-content="left"
       block
       color="info"
       onClick={showPostModal}
-      >
-      <div key={post.id} >
-        <ul style={{listStyleType: 'none', padding: 0, margin: 0}}>
-        <li style={{fontSize: 19, listStyleType: 'none'}}>{post.title}</li>
+    >
+      <div key={post.id}>
+        <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
+          <li style={{ fontSize: 19, listStyleType: "none" }}>{post.title}</li>
         </ul>
       </div>
     </Button>
   );
-}
+};
+export default Post;
